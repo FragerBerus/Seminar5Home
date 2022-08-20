2e215004e25569a1822aa2ad7a4b83c00497be65
Berus Frager <gitmailru@yandex.ru>
2022-08-20 11:41:47 +0300

    Коммит файлов папки проекта Example036

---
+++ Example036/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 36:
+// Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+int Sum = 0;
+int[] ArrayNumbers = new int[100];
+for (int i = 0; i < 100; i++)
+{
+    ArrayNumbers[i] = new Random().Next(0, 99);
+}
+int index = 1;
+while (index < 100)
+{
+    Sum = Sum + ArrayNumbers[index];
+    index = index + 2;
+}
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях, равна {Sum}.");
